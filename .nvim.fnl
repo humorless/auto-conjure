48bb896d26bae5876c05462bf4d0b1ec4a7059f9
Laurence Chen <laurence@replware.dev>
2025-08-14 00:00:47 +0800

    add auto-conjure

---
+++ .nvim.fnl
@@ -0,0 +1,4 @@
+;; Use `:set exrc` in Neovim to enable 
+;; `Directory local Neovim configuration`
+
+(set package.path (.. package.path ";./lua/?.lua"))
---
+++ .nvim.lua
@@ -0,0 +1,3 @@
+-- [nfnl] Compiled from .nvim.fnl by https://github.com/Olical/nfnl, do not edit.
+package.path = (package.path .. ";./lua/?.lua")
+return nil
